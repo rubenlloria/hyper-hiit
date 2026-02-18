b7f03455b52c8b9eb9754dfe7908c6f1a9300468
Orion <orion@aicorp.com>
2026-02-19 00:34:55 +0100

    feat: change chronometer logic to count up from zero

--- src/Chronometer.cpp
+++ src/Chronometer.cpp
@@ -1,18 +1,45 @@
 #include "Chronometer.h"
-#include <QTime>
+#include <QDebug>
 
-Chronometer::Chronometer(QObject *parent) : QObject(parent) {
-    m_refreshTimer = new QTimer(this);
-    connect(m_refreshTimer, &QTimer::timeout, this, &Chronometer::timeChanged);
+Chronometer::Chronometer(QObject *parent)
+    : QObject(parent), m_totalTargetMs(0), m_timeText("00:00:00")
+{
+    m_timer = new QTimer(this);
+    m_timer->setInterval(40);
+    connect(m_timer, &QTimer::timeout, this, &Chronometer::updateTime);
 }
 
-void Chronometer::start() {
-    m_timer.start();
-    m_refreshTimer->start(100); // Update every 100ms
+void Chronometer::start(int seconds) {
+    // We ignore 'seconds' for now as we are counting UP from zero
+    Q_UNUSED(seconds);
+
+    m_elapsedTimer.start(); // Resets and starts the timer from 0
+    m_timer->start();
+
+    qDebug() << "Chronometer started (Count-up mode)";
 }
 
-QString Chronometer::timeDisplay() const {
-    if (!m_timer.isValid()) return "00:00.0";
-    qint64 ms = m_timer.elapsed();
-    return QTime::fromMSecsSinceStartOfDay(ms).toString("mm:ss.z").left(7);
+void Chronometer::stop() {
+    m_timer->stop();
+}
+
+void Chronometer::updateTime() {
+    // Get the actual elapsed time since start() was called
+    int elapsed = static_cast<int>(m_elapsedTimer.elapsed());
+
+    // Update the text with the elapsed time
+    formatTimeText(elapsed);
+}
+
+void Chronometer::formatTimeText(int totalMs) {
+    int mins = (totalMs / 60000) % 60;
+    int secs = (totalMs / 1000) % 60;
+    int msecs = (totalMs % 1000) / 10;
+
+    m_timeText = QString("%1:%2:%3")
+                     .arg(mins, 2, 10, QChar('0'))
+                     .arg(secs, 2, 10, QChar('0'))
+                     .arg(msecs, 2, 10, QChar('0'));
+
+    emit timeTextChanged();
 }
